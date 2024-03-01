460e27f1715a3074167e5651bf652760829a4b77
Sithabo <sithabisoh@muwci.net>
2024-03-01 17:36:17 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,158 @@
-import Image from 'next/image'
+'use client'
+import Image from "next/image";
+import logo from "../../public/logo.png";
+import banner from "../../public/banner.jpg";
+import Link from "next/link";
+import { Footer } from "flowbite-react";
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="px-16">
+      {/* HEADER */}
+      <Header />
+      <Banner />
+      <Features />
+      <Gallery />
+      <ShowCase />
+      <WebFooter />
+    </main>
+  );
+}
+
+function Header() {
+  return (
+    <nav className="flex justify-between py-10">
+      <div className="flex items-center">
+        <Image
+          src={logo}
+          width={50}
+          height={50}
+          alt="Lunchbox, 'THE' meeting place for restaurant lovers"
+        />
+        <span className="text-xl font-semibold">Lunchbox</span>
+      </div>
+      <div>
+        <Link href="/">
+          <button className="p-3 m-1">About</button>
+        </Link>
+        <Link href="/">
+          <button className="p-3 m-1 bg-[#1e1e1e] text-white">Download</button>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+function Banner() {
+  return (
+    <div className="flex justify-between items-center py-10">
+      {/* FIRST SECTION OF BANNER */}
+      <div className=" w-1/2">
+        <h2 className=" text-8xl font-medium">Connecting food lovers</h2>
+        <div className=" mt-5">
+          <p>Track Restaurants</p>
+          <p>Save those you want to see</p>
+          <p>Tell your friends what’s good.</p>
+        </div>
+        <div className=" mt-5">
+          <Link href="/">
+            <button className="p-3 mr-2 bg-[#1e1e1e] text-white">
+              Coming soon to Android
+            </button>
+          </Link>
+          <Link href="/">
+            <button className="p-3 mr-2 bg-[#1e1e1e] text-white">
+              Coming soon to IOS
+            </button>
+          </Link>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* SECOND SECTION OF BANNER */}
+      <div className=" w-1/2">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={banner}
+          alt="Lunchbox banner: 'The' meeting place for restaurant enthusiasts"
         />
       </div>
+    </div>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function Features() {
+  const features = [
+    { title: "Create anything", description: "Welcome to a world of limitless possibilities, where the journey is as exhilarating as the destination, and where every moment is an opportunity to make your mark on the canvas of existence. The only limit is the extent of your imagination." },
+    { title: "Create anything", description: "Welcome to a world of limitless possibilities, where the journey is as exhilarating as the destination, and where every moment is an opportunity to make your mark on the canvas of existence. The only limit is the extent of your imagination." },
+    { title: "Create anything", description: "Welcome to a world of limitless possibilities, where the journey is as exhilarating as the destination, and where every moment is an opportunity to make your mark on the canvas of existence. The only limit is the extent of your imagination." },
+  ];
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  return (
+    <div className="grid grid-cols-3 gap-20 py-20">
+      {features.map((item) => {
+        return (
+          <div className=" min-w-60">
+            <h2 className=" text-xl font-semibold">{item.title}</h2>
+            <p className=" text-justify pt-5">{item.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+function Gallery () {
+  const images = [banner, banner, banner, banner]
+  return (
+    <div className="flex flex-col items-center py-20">
+        <h2 className=" text-5xl pb-10 font-semibold">Gallery</h2>
+        <span className=" pb-10">The only limit is the extent of your imagination.</span>
+        <div className="grid grid-cols-4 gap-10">
+          { images.map(
+            item => {
+              return (
+                <Image src={item} alt="Lunchbox App Images snapshots"></Image>
+              )
+            }
+          ) }
+        </div>
+    </div>
   )
+}
+
+function ShowCase () {
+  return (
+    <div className="flex flex-col items-center py-20">
+      <h2 className="text-5xl pb-10 font-semibold">Discover a world of possibilities</h2>
+      <div className=" mt-5">
+          <Link href="/">
+            <button className="p-3 mr-2 bg-[#1e1e1e] text-white">
+              Coming soon to Android
+            </button>
+          </Link>
+          <Link href="/">
+            <button className="p-3 mr-2 bg-[#1e1e1e] text-white">
+              Coming soon to IOS
+            </button>
+          </Link>
+        </div>
+    </div>
+  )
+}
+
+function WebFooter() {
+  return (
+    <Footer container>
+      <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="Lunchbox™" year={2024} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+      </div>
+    </Footer>
+  );
 }
