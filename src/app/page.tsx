d407463e90c7cb95563b5f469bcc0fb1ee1458e6
Sithabo <sithabisoh@muwci.net>
2024-04-04 02:22:50 +0200

    fixed failed deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <Header />
       <Banner />
       <Features />
-      <Gallery />
+      {/* <Gallery /> */}
       <ShowCase />
       <WebFooter />
     </main>
